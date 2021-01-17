37ff3028812ba9e82b2f973ec8dba9cfa7423ef4
bwcompany <han5-1@hotmail.com>
2021-01-17 19:22:45 +0300

    Util changes

--- src/com/bwcompany/util/Util.java
+++ src/com/bwcompany/util/Util.java
@@ -1,13 +1,35 @@
 package com.bwcompany.util;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 //Logical static classes
 public final class Util {
+    private static final Random MS_RANDOM;
+    private static final String MS_ALPHABET_TR = "abcçdefgğhıijklmnoöprsştuüvyz";
     //...
+
+    static{
+        MS_RANDOM = new Random();
+    }
 
     private Util() {}
 
     public static String capitalize(String s)
     {
         return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+    }
+
+    public static String getRandomStringTR(int n)
+    {
+        char[] randomChars = new char[n];
+
+        IntStream.range(0, n)
+                .forEach(index -> randomChars[index]
+                        = MS_ALPHABET_TR.charAt(MS_RANDOM.nextInt(29)));
+
+        return new String(randomChars);
     }
 }
